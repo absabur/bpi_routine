7d839ed5f71a9536c2dbe11f8a8af72e1a534598
abdus-sabur <absabur929@gmail.com>
2023-06-30 17:44:17 +0600

    add cache

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,19 +1,19 @@
-// export default function serviceWorker () {
-//     let swUrl = `${process.env.PUBLIC_URL}/sw.js`
+export default function serviceWorker () {
+    let swUrl = `${process.env.PUBLIC_URL}/sw.js`
 
-//     if ( `serviceWorker` in navigator) {
-//         window.addEventListener('load', function(){
-//             navigator.serviceWorker.register(swUrl)
-//             .then(function(registration){
-//                 console.log("Worker Registration is successfull.",registration.scop);
-//             },function(err){
-//                 console.log("Failed");
-//             })
-//             .catch(function(err){
-//                 console.log(err);
-//             })
-//         })
-//     }else {
-//         console.log("Service worker is not supported.");
-//     }
-// }
+    if ( `serviceWorker` in navigator) {
+        window.addEventListener('load', function(){
+            navigator.serviceWorker.register(swUrl)
+            .then(function(registration){
+                console.log("Worker Registration is successfull.",registration.scop);
+            },function(err){
+                console.log("Failed");
+            })
+            .catch(function(err){
+                console.log(err);
+            })
+        })
+    }else {
+        console.log("Service worker is not supported.");
+    }
+}
