d318ef7e6cda5c2797cd53b2e8e1f2162ab8c564
abdus-sabur <absabur929@gmail.com>
2023-06-30 20:20:00 +0600

    d

--- public/sw.js
+++ public/sw.js
@@ -49,9 +49,6 @@ self.addEventListener('fetch', (event) => {
 
 // This is the "Offline copy of pages" service worker
 
-const CACHE = "pwabuilder-offline";
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -70,12 +67,9 @@ workbox.routing.registerRoute(
 
 // This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-const CACHE = "pwabuilder-offline-page";
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
