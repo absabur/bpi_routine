67bb08f366cd2a8f3778ca976d2878ecbccf56c7
abdus-sabur <absabur929@gmail.com>
2023-07-07 20:00:54 +0600

    mt added correct day

--- src/Routines/mt/mt_2_4c.js
+++ src/Routines/mt/mt_2_4c.js
@@ -1,119 +1,121 @@
+import { GiTireIronCross } from "react-icons/gi";
+
 const routineData = [
-    [
-      {
-        p: 'p1',
-        subject: 'SUNDAY',
-        room: ''
-      },{
-        p: 'p3',
-        subject: 'SUNDAY',
-        room: 'SUNDAY'
-      },{
-        p: 'p1',
-        subject: 'SUNDAY',
-        room: 'SUNDAY'
-      },{
-        p: 'p1',
-        subject: 'SUNDAY',
-        room: 'SUNDAY'
-      },{
-        p: 'p2',
-        subject: 'SUNDAY',
-        room: 'SUNDAY'
-      },
-    ],
-    [
-      {
-        p: 'p1',
-        subject: 'SUNDAY',
-        room: ''
-      },{
-        p: 'p3',
-        subject: 'SUNDAY',
-        room: 'SUNDAY'
-      },{
-        p: 'p1',
-        subject: 'SUNDAY',
-        room: 'SUNDAY'
-      },{
-        p: 'p1',
-        subject: 'SUNDAY',
-        room: 'SUNDAY'
-      },{
-        p: 'p2',
-        subject: 'SUNDAY',
-        room: 'SUNDAY'
-      },
-    ],
-    [
-      {
-        p: 'p1',
-        subject: 'SUNDAY',
-        room: ''
-      },{
-        p: 'p3',
-        subject: 'SUNDAY',
-        room: 'SUNDAY'
-      },{
-        p: 'p1',
-        subject: 'SUNDAY',
-        room: 'SUNDAY'
-      },{
-        p: 'p1',
-        subject: 'SUNDAY',
-        room: 'SUNDAY'
-      },{
-        p: 'p2',
-        subject: 'SUNDAY',
-        room: 'SUNDAY'
-      },
-    ],
-    [
-      {
-        p: 'p1',
-        subject: 'SUNDAY',
-        room: ''
-      },{
-        p: 'p3',
-        subject: 'SUNDAY',
-        room: 'SUNDAY'
-      },{
-        p: 'p1',
-        subject: 'SUNDAY',
-        room: 'SUNDAY'
-      },{
-        p: 'p1',
-        subject: 'SUNDAY',
-        room: 'SUNDAY'
-      },{
-        p: 'p2',
-        subject: 'SUNDAY',
-        room: 'SUNDAY'
-      },
-    ],
-    [
-      {
-        p: 'p1',
-        subject: 'SUNDAY',
-        room: ''
-      },{
-        p: 'p3',
-        subject: 'SUNDAY',
-        room: 'SUNDAY'
-      },{
-        p: 'p1',
-        subject: 'SUNDAY',
-        room: 'SUNDAY'
-      },{
-        p: 'p1',
-        subject: 'SUNDAY',
-        room: 'SUNDAY'
-      },{
-        p: 'p2',
-        subject: 'SUNDAY',
-        room: 'SUNDAY'
-      },
-    ],
-  ]
-  
-  export default routineData;
+  [
+    {
+      p: 'p1',
+      subject: 'SUNDAY',
+      room: ''
+    },{
+      p: 'p3',
+      subject: 'SUNDAY',
+      room: 'SUNDAY'
+    },{
+      p: 'p1',
+      subject: 'SUNDAY',
+      room: 'SUNDAY'
+    },{
+      p: 'p1',
+      subject: 'SUNDAY',
+      room: 'SUNDAY'
+    },{
+      p: 'p2',
+      subject: <GiTireIronCross />,
+      room: ''
+    },
+  ],
+  [
+    {
+      p: 'p1',
+      subject: 'MONDAY',
+      room: ''
+    },{
+      p: 'p3',
+      subject: 'SUNDAY',
+      room: 'SUNDAY'
+    },{
+      p: 'p1',
+      subject: 'SUNDAY',
+      room: 'SUNDAY'
+    },{
+      p: 'p1',
+      subject: 'SUNDAY',
+      room: 'SUNDAY'
+    },{
+      p: 'p2',
+      subject: 'SUNDAY',
+      room: 'SUNDAY'
+    },
+  ],
+  [
+    {
+      p: 'p1',
+      subject: 'TUESDAY',
+      room: ''
+    },{
+      p: 'p3',
+      subject: 'SUNDAY',
+      room: 'SUNDAY'
+    },{
+      p: 'p1',
+      subject: 'SUNDAY',
+      room: 'SUNDAY'
+    },{
+      p: 'p1',
+      subject: 'SUNDAY',
+      room: 'SUNDAY'
+    },{
+      p: 'p2',
+      subject: 'SUNDAY',
+      room: 'SUNDAY'
+    },
+  ],
+  [
+    {
+      p: 'p1',
+      subject: 'WEDNESDAY',
+      room: ''
+    },{
+      p: 'p3',
+      subject: 'SUNDAY',
+      room: 'SUNDAY'
+    },{
+      p: 'p1',
+      subject: 'SUNDAY',
+      room: 'SUNDAY'
+    },{
+      p: 'p1',
+      subject: 'SUNDAY',
+      room: 'SUNDAY'
+    },{
+      p: 'p2',
+      subject: 'SUNDAY',
+      room: 'SUNDAY'
+    },
+  ],
+  [
+    {
+      p: 'p1',
+      subject: 'THURSDAY',
+      room: ''
+    },{
+      p: 'p3',
+      subject: 'SUNDAY',
+      room: 'SUNDAY'
+    },{
+      p: 'p1',
+      subject: 'SUNDAY',
+      room: 'SUNDAY'
+    },{
+      p: 'p1',
+      subject: 'SUNDAY',
+      room: 'SUNDAY'
+    },{
+      p: 'p2',
+      subject: 'SUNDAY',
+      room: 'SUNDAY'
+    },
+  ],
+]
+
+export default routineData;
