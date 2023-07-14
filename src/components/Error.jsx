b17bf5c689ee1fd1cb7cda68f1caf35c74146a7c
absabur <absabur929@gmail.com>
2023-07-14 15:08:57 +0600

    error page design

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,16 +1,22 @@
 import React from "react";
+import './error.css'
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
     <div
-      className="color-sec"
-      style={{ padding: "2rem 0.3rem", marginBottom: "-2rem" }}
+      style={{ padding: "2rem 0.3rem", paddingBottom: "2rem" , backgroundColor:"#416475"}}
     >
-      {" "}
-      <h1>
-        404 page not found. <br /> Invalid link URL.
-      </h1>
-    </div>
+      <h1>404 Error Page, page not found</h1>
+        <section className="error-container">
+          <span>4</span>
+          <span><span className="screen-reader-text">0</span></span>
+          <span>4</span>
+        </section>
+        <div className="link-container">
+          <Link className="home-link" to="/">Go to Home Page</Link>
+        </div>
+      </div>
   );
 };
 
