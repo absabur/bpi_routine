17850d33f32beb2d7e5e9b682bbe7a2db54cd406
absabur <absabur929@gmail.com>
2023-08-24 03:14:17 +0600

    data correction and civil 2a, 2b

--- src/Routines/civil/ct_1_2b.js
+++ src/Routines/civil/ct_1_2b.js
@@ -3,131 +3,170 @@ import { GiTireIronCross } from "react-icons/gi";
 const routineData = [
   [
     {
-      p: 'p1',
-      subject: 'SUNDAY',
-      room: ''
-    },{
-      p: 'p3',
-      subject: 'DEMO',
-      room: 'DEMO'
-    },{
-      p: 'p1',
-      subject: 'DEMO',
-      room: 'DEMO'
-    },{
-      p: 'p1',
-      subject: 'DEMO',
-      room: 'DEMO'
-    },{
-      p: 'p2',
+      p: "p1",
+      subject: "SUNDAY",
+      code: "",
+      room: "",
+    },
+    {
+      p: "p3",
+      subject: "Civil Engineering Drawing",
+      code: "26421",
+      room: "310",
+    },
+    {
+      p: "p1",
+      subject: "Bangla -II",
+      code: "25721",
+      room: "310",
+    },
+    {
+      p: "p2",
+      subject: "Mathematics-II",
+      code: "25921",
+      room: "307",
+    },
+    {
+      p: "p1",
+      subject: "English-II",
+      code: "25722",
+      room: "307",
+    },
+  ],
+
+// ==============================
+
+  [
+    {
+      p: "p1",
+      subject: "MONDAY",
+      code: "",
+      room: "",
+    },
+    {
+      p: "p3",
+      subject: "Physical Education & Life Skills Development",
+      code: "25812",
+      room: "Field",
+    },
+    {
+      p: "p1",
+      subject: "Mathematics-II",
+      code: "25921",
+      room: "310",
+    },
+    {
+      p: "p3",
+      subject: "Civil Engineering Drawing",
+      code: "26421",
+      room: "310",
+    },
+  ],
+
+// ==============================
+
+  [
+    {
+      p: "p1",
+      subject: "TUESDAY",
+      code: "",
+      room: "",
+    },
+    {
+      p: "p3",
+      subject: "Basic Workshop Practice",
+      code: "27011",
+      room: "112",
+    },
+    {
+      p: "p1",
+      subject: "Basic Electronics",
+      code: "26811",
+      room: "302",
+    },
+    {
+      p: "p3",
+      subject: "Physics -I",
+      code: "25912",
+      room: "301",
+    },
+  ],
+
+// ==============================
+
+  [
+    {
+      p: "p1",
+      subject: "WEDNESDAY",
+      code: "",
+      room: "",
+    },
+    {
+      p: "p2",
+      subject: "Physics -I",
+      code: "25912",
+      room: "307",
+    },
+    {
+      p: "p1",
+      subject: "Civil Engineering Drawing",
+      code: "26421",
+      room: "307",
+    },
+    {
+      p: "p1",
+      subject: "Bangla -II",
+      code: "25721",
+      room: "311",
+    },
+    {
+      p: "p3",
+      subject: "Basic Electronics",
+      code: "26811",
+      room: "311",
+    },
+  ],
+
+// ==============================
+
+  [
+    {
+      p: "p1",
+      subject: "THURSDAY",
+      code: "",
+      room: "",
+    },
+    {
+      p: "p3",
+      subject: "Mathematics-II",
+      code: "25921",
+      room: "308",
+    },
+    {
+      p: "p1",
+      subject: "Physics -I",
+      code: "25912",
+      room: "308",
+    },
+    {
+      p: "p1",
+      subject: "English-II",
+      code: "25722",
+      room: "307",
+    },
+    {
+      p: "p1",
+      subject: "Basic Electronics",
+      code: "26811",
+      room: "307",
+    },
+    {
+      p: "p1",
       subject: <GiTireIronCross style={{fontSize: "1.8rem"}}/>,
-      room: ''
+      code: "",
+      room: "",
     },
   ],
-
-// ==============================
-
-  [
-    {
-      p: 'p1',
-      subject: 'MONDAY',
-      room: ''
-    },{
-      p: 'p3',
-      subject: 'DEMO',
-      room: 'DEMO'
-    },{
-      p: 'p1',
-      subject: 'DEMO',
-      room: 'DEMO'
-    },{
-      p: 'p1',
-      subject: 'DEMO',
-      room: 'DEMO'
-    },{
-      p: 'p2',
-      subject: 'DEMO',
-      room: 'DEMO'
-    },
-  ],
-
-// ==============================
-
-  [
-    {
-      p: 'p1',
-      subject: 'TUESDAY',
-      room: ''
-    },{
-      p: 'p3',
-      subject: 'DEMO',
-      room: 'DEMO'
-    },{
-      p: 'p1',
-      subject: 'DEMO',
-      room: 'DEMO'
-    },{
-      p: 'p1',
-      subject: 'DEMO',
-      room: 'DEMO'
-    },{
-      p: 'p2',
-      subject: 'DEMO',
-      room: 'DEMO'
-    },
-  ],
-
-// ==============================
-
-  [
-    {
-      p: 'p1',
-      subject: 'WEDNESDAY',
-      room: ''
-    },{
-      p: 'p3',
-      subject: 'DEMO',
-      room: 'DEMO'
-    },{
-      p: 'p1',
-      subject: 'DEMO',
-      room: 'DEMO'
-    },{
-      p: 'p1',
-      subject: 'DEMO',
-      room: 'DEMO'
-    },{
-      p: 'p2',
-      subject: 'DEMO',
-      room: 'DEMO'
-    },
-  ],
-
-// ==============================
-
-  [
-    {
-      p: 'p1',
-      subject: 'THURSDAY',
-      room: ''
-    },{
-      p: 'p3',
-      subject: 'DEMO',
-      room: 'DEMO'
-    },{
-      p: 'p1',
-      subject: 'DEMO',
-      room: 'DEMO'
-    },{
-      p: 'p1',
-      subject: 'DEMO',
-      room: 'DEMO'
-    },{
-      p: 'p2',
-      subject: 'DEMO',
-      room: 'DEMO'
-    },
-  ],
-]
+];
 
 export default routineData;
